ee6aa1421ea93f6f9156f7ff8e611029c0001ac8
jek01680 <jek01680@icloud.com>
2022-04-13 12:31:46 +0900

    Initial Commit

---
+++ Outstagram/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Outstagram
+//
+//  Created by 정은경 on 2022/04/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
